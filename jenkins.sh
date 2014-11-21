534d4b59899ae2bc0ce73fdeaaea17528f29427d
CristinaGlz <cgm00001@gmail.com>
2014-11-21 08:59:00 -0600

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,6 @@
 #! /bin/sh
 
-sudo sh -c "echo 'deb http://badgerports.org lucid main #Mono' >> /etc/apt/sources.list.d/Mono.list" && gpg --keyserver keyserver.ubuntu.com --recv 0E1FAD0C && gpg --export --armor 0E1FAD0C | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install mono
-sudo apt-get install monodevelop
+sudo apt-get install mono-complete
 
 xbuild TestProject2.sln
 nunit-console TestProject2/bin/Debug/TestProject2.dll
