98d796c443541544956f467232b8254b4967dbd9
CristinaGlz <cgm00001@gmail.com>
2014-11-21 08:47:29 -0600

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,7 @@
 #! /bin/sh
 
-apt-key adv --keyserver pgp.mit.edu --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+
 echo "deb http://download.mono-project.com/repo/debian wheezy main" > /etc/apt/sources.list.d/mono-xamarin.list
-
-
 
 xbuild TestProject2.sln
 nunit-console TestProject2/bin/Debug/TestProject2.dll
