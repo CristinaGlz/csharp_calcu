4c2161f8df86591ae33ea6fd57073b2fc6ba3dbc
CristinaGlz <cgm00001@gmail.com>
2014-11-21 08:51:58 -0600

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
-
-echo "deb http://download.mono-project.com/repo/debian wheezy main" > /etc/apt/sources.list.d/mono-xamarin.list
+sudo sh -c "echo 'deb http://badgerports.org lucid main #Mono' >> /etc/apt/sources.list.d/Mono.list" && gpg --keyserver keyserver.ubuntu.com --recv 0E1FAD0C && gpg --export --armor 0E1FAD0C | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install mono
+sudo apt-get install monodevelop
 
 xbuild TestProject2.sln
 nunit-console TestProject2/bin/Debug/TestProject2.dll
