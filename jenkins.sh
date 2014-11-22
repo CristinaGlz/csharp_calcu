0b8578c23af8193073e33b697fc29d8c26bd46f3
CristinaGlz <cgm00001@gmail.com>
2014-11-21 22:34:44 -0600

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
+sudo aptitude update
 
+sudo aptitude install mono-runtime
+
+sudo apt-get update 
 sudo apt-get install mono-complete
+
+
 
 xbuild TestProject2.sln
 nunit-console TestProject2/bin/Debug/TestProject2.dll
